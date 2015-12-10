1711a07cf9fe74d1979160b322a1d0b0056b5459
bkonkle <brandon@konkle.us>
2015-12-10 14:48:53 -0700

    v3.0.0 - move side effects out of the reducer

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,13 @@
 import {createAction} from 'redux-actions'
 import {NAVIGATE} from './constants'
 
-export const navigate = createAction(NAVIGATE)
+export const navigate = createAction(NAVIGATE, payload => {
+  if (typeof history !== 'undefined') {
+    if (payload.push) {
+      history.pushState({}, null, payload.url)
+    } else if (payload.replace) {
+      history.replaceState({}, null, payload.url)
+    }
+  }
+  return payload
+})
--- src/reducer.js
+++ src/reducer.js
@@ -13,11 +13,6 @@ export function routeState(url) {
 
 export default handleActions({
   [NAVIGATE]: (state, action) => {
-    if (action.payload.push) {
-      history.pushState({}, null, action.payload.url)
-    } else if (action.payload.replace) {
-      history.replaceState({}, null, action.payload.url)
-    }
     return {...state, ...routeState(action.payload.url)}
   },
 }, initialState)
