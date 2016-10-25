0fbc140422fd53790a4e64d7c82e36ac2adf27fb
bkonkle <brandon@konkle.us>
2016-10-25 09:44:18 -0600

    Put the actions back the way they were

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,9 @@
+/* global history */
 import {createAction} from 'redux-actions'
 import {NAVIGATE} from './constants'
 
-const history = typeof window !== 'undefined' && window.history
-
 export const navigate = createAction(NAVIGATE, payload => {
-  if (history && payload.source !== 'popState') {
+  if (typeof history !== 'undefined' && payload.source !== 'popState') {
     if (payload.push) {
       history.pushState({}, null, payload.url)
     } else if (payload.replace) {
